e9c3a2cfc1513860dd2f700a783228328acfee1c
unknown <brunoeduds84@gmail.com>
2025-06-03 22:46:14 -0300

    implementado exibição de detalhes da tarefa

--- src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
+++ src/br/com/poo/gestaotarefas/tarefa/Tarefa.java
@@ -10,4 +10,9 @@ public class Tarefa {
         this.descricao = descricao;
         this.concluida = concluida;
     }
+
+    public String obterDados(){
+        return String.format("ID: %d | Descricao: %s | Status: %s", this.id, this.descricao, this.concluida ? "Concluida" : "Em aberto");
+    }
+
 }
